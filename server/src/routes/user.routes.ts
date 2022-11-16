1cd809e2d2fdea55d63e6fdf18c510a27cd192dc
Brendon <brendon.lopes.21@gmail.com>
2022-11-16 18:27:03 -0300

    feat(server): :sparkles: add login route to user routes on '/user/login'

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import CreateUserController from '../factories/create-user-controller'
 import ValidateCreateUser from '../middlewares/validate-create-user'
+import ValidateLogin from '../middlewares/validate-login'
 
 const userRouter = Router()
 
@@ -10,6 +11,12 @@ userRouter.post(
   '/',
   (req, res, next) => ValidateCreateUser.validate(req, res, next),
   async (req, res) => await userController.create(req, res)
+)
+
+userRouter.post(
+  '/login',
+  (req, res, next) => ValidateLogin.validate(req, res, next),
+  async (req, res) => await userController.login(req, res)
 )
 
 export default userRouter
