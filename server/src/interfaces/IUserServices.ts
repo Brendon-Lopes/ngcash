86572936dc5317b0f80c3fdb81fb5131e2686d9c
Brendon <brendon.lopes.21@gmail.com>
2022-11-16 18:25:56 -0300

    feat(server): :sparkles: add 'login' method to IUserServices

--- server/src/interfaces/IUserServices.ts
+++ server/src/interfaces/IUserServices.ts
@@ -1,6 +1,9 @@
 import { User } from '@prisma/client'
 import ICreateUserResponse from './ICreateUserResponse'
+import ILoginData from './ILoginData'
+import ILoginResponse from './ILoginResponse'
 
 export default interface IUserServices {
   create: (user: User) => Promise<ICreateUserResponse>
+  login: (data: ILoginData) => Promise<ILoginResponse>
 }
