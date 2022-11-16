88b29a88053e45f7f7df8e32acc849e199de6698
Brendon <brendon.lopes.21@gmail.com>
2022-11-16 17:53:35 -0300

    feat(server): :sparkles: add TokenHandler class
    to handle JWT tokens creation/validation

---
+++ server/src/utils/jwt/index.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config'
+import jwt from 'jsonwebtoken'
+import CustomError from '../error-handling/custom-error'
+import statusCodes from 'http-status-codes'
+
+class TokenHandler {
+  constructor (private readonly secret: string) { }
+
+  public createToken (payload: any): string {
+    return jwt.sign(payload, this.secret)
+  }
+
+  public verifyToken (token: string): string | jwt.JwtPayload | undefined {
+    try {
+      return jwt.verify(token, this.secret)
+    } catch (error) {
+      throw new CustomError(statusCodes.UNAUTHORIZED, 'invalid token')
+    }
+  }
+}
+
+const jwtSecret = process.env.JWT_SECRET as string
+
+const tokenHandler = new TokenHandler(jwtSecret)
+
+export default tokenHandler
