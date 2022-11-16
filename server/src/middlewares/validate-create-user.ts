f4505f78ba49494bcae137f84925a30f42b2d897
Brendon <brendon.lopes.21@gmail.com>
2022-11-16 17:23:12 -0300

    feat(server): :sparkles: add validation when creating new user

---
+++ server/src/middlewares/validate-create-user.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+import ZodValidation from '../utils/zod-validator'
+
+export default class ValidateCreateUser extends ZodValidation {
+  static schema = z.object({
+    username: z
+      .string({ required_error: 'username is required', invalid_type_error: 'username must be a string' })
+      .min(3, 'username must be at least 3 characters long')
+      .max(30, 'username must be at most 30 characters long'),
+
+    password: z
+      .string({ required_error: 'password is required', invalid_type_error: 'password must be a string' })
+      .regex(
+        /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,20}$/,
+        'password must be between 8 and 20 characters long and contain at least one uppercase letter, one lowercase letter and one number'
+      )
+  })
+}
