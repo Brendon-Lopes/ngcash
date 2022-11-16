ee5a658701dba551a377ce8858e2a838a5a21fe4
Brendon <brendon.lopes.21@gmail.com>
2022-11-16 17:52:52 -0300

    feat(server): :sparkles: update UserServices create method
    add validation for username, hash password, token creation, and change return

--- server/src/services/user-services.ts
+++ server/src/services/user-services.ts
@@ -1,18 +1,44 @@
 import { PrismaClient, User } from '@prisma/client'
 import IUserServices from '../interfaces/IUserServices'
+import CustomError from '../utils/error-handling/custom-error'
+import statusCodes from 'http-status-codes'
+import PasswordHandler from '../utils/password-handler'
+import ICreateUserResponse from '../interfaces/ICreateUserResponse'
+import tokenHandler from '../utils/jwt'
 
 export default class UserServices implements IUserServices {
   constructor (private readonly prisma: PrismaClient) {}
 
-  async create (data: User): Promise<User> {
-    return await this.prisma.user.create({
+  async create (data: User): Promise<ICreateUserResponse> {
+    await this._verifyUsername(data.username)
+
+    const password = await PasswordHandler.hash(data.password)
+
+    const user = await this.prisma.user.create({
       data: {
         username: data.username,
-        password: data.password,
+        password,
         account: {
           create: { }
         }
       }
     })
+
+    const token = tokenHandler.createToken({ id: user.id, username: user.username })
+
+    return {
+      id: user.id,
+      username: user.username,
+      accountId: user.accountId,
+      token
+    }
+  }
+
+  private async _verifyUsername (username: string): Promise<void> {
+    const user = await this.prisma.user.findUnique({
+      where: { username }
+    })
+
+    if (user !== null) throw new CustomError(statusCodes.CONFLICT, 'Username already exists')
   }
 }
