2c8b7dd055aa9b98e0baeafd3ebb761d01e0528c
Brendon <brendon.lopes.21@gmail.com>
2022-11-16 17:50:44 -0300

    feat(server): :sparkles: update interface IUserServices

--- server/src/interfaces/IUserServices.ts
+++ server/src/interfaces/IUserServices.ts
@@ -1,5 +1,6 @@
 import { User } from '@prisma/client'
+import ICreateUserResponse from './ICreateUserResponse'
 
 export default interface IUserServices {
-  create: (user: User) => Promise<User>
+  create: (user: User) => Promise<ICreateUserResponse>
 }
