43129fcee2c6fc2aa5eaf6f161b0d224c5e5dd5c
Brendon <brendon.lopes.21@gmail.com>
2022-11-17 17:16:48 -0300

    test(server): :clown_face: update mocks

--- server/src/utils/mocks/user-mocks.ts
+++ server/src/utils/mocks/user-mocks.ts
@@ -16,3 +16,19 @@ export const createUserResponseMock = {
   accountId: 'clak46hxp0001o89q4byflyd5',
   token: 'validtoken'
 }
+
+export const readOneUserPrismaResponseMock = {
+  id: 'clak46hxp0000o89qiv0tiggt',
+  username: 'username',
+  accountId: 'clak46hxp0001o89q4byflyd5',
+  account: {
+    balance: 100
+  }
+}
+
+export const readOneUserResponseMock = {
+  id: 'clak46hxp0000o89qiv0tiggt',
+  username: 'username',
+  accountId: 'clak46hxp0001o89q4byflyd5',
+  balance: 100
+}
