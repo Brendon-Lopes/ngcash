5074a34c8e7f7318853f9f2ed28612a73a7872af
Brendon <brendon.lopes.21@gmail.com>
2022-11-16 21:20:59 -0300

    refactor(server): :recycle: replace hardcoded status codes with lib 'http-status-codes'

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -1,16 +1,17 @@
 import { Request, Response } from 'express'
 import IUserServices from '../interfaces/IUserServices'
+import statusCodes from 'http-status-codes'
 
 export default class UserController {
   constructor (private readonly userServices: IUserServices) {}
 
   async create (req: Request, res: Response): Promise<Response> {
     const user = await this.userServices.create(req.body)
-    return res.status(201).json(user)
+    return res.status(statusCodes.CREATED).json(user)
   }
 
   async login (req: Request, res: Response): Promise<Response> {
     const user = await this.userServices.login(req.body)
-    return res.status(200).json(user)
+    return res.status(statusCodes.OK).json(user)
   }
 }
