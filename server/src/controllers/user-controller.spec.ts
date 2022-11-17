fa268d201917e826910c4bcec33c7f747d94131d
Brendon <brendon.lopes.21@gmail.com>
2022-11-16 21:23:18 -0300

    test(server): :white_check_mark: add tests for UserController methods

---
+++ server/src/controllers/user-controller.spec.ts
@@ -0,0 +1,35 @@
+import sinon from 'sinon'
+import chai from 'chai'
+import UserController from './user-controller'
+import statusCodes from 'http-status-codes'
+import UserServices from '../services/user-services'
+import prismaClient from '../database/prismaClient'
+import { Request, Response } from 'express'
+import { createUserResponseMock } from '../utils/mocks/user-mocks'
+
+const { expect } = chai
+
+describe('User Controller', () => {
+  const userServices = new UserServices(prismaClient)
+  const userController = new UserController(userServices)
+
+  const req = {} as Request
+  const res = {} as Response
+
+  before(() => {
+    res.status = sinon.stub().returns(res)
+    res.json = sinon.stub()
+  })
+
+  describe('Create method', () => {
+    afterEach(() => sinon.restore())
+  })
+
+  it('should create a new user', async () => {
+    sinon.stub(userServices, 'create').resolves(createUserResponseMock)
+
+    await userController.create(req, res)
+    expect((res.status as sinon.SinonStub).calledWith(statusCodes.CREATED)).to.be.true
+    expect((res.json as sinon.SinonStub).calledWith(createUserResponseMock)).to.be.true
+  })
+})
