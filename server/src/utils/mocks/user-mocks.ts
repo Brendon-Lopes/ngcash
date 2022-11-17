0193c0aefb9aa7434ee0bd1b32bc6e2a7bd46eaa
Brendon <brendon.lopes.21@gmail.com>
2022-11-16 21:22:43 -0300

    test(server): :clown_face: add mocks for testing

---
+++ server/src/utils/mocks/user-mocks.ts
@@ -0,0 +1,18 @@
+export const createUserPrismaResponseMock = {
+  id: 'clak46hxp0000o89qiv0tiggt',
+  username: 'username',
+  password: 'Password1',
+  accountId: 'clak46hxp0001o89q4byflyd5'
+}
+
+export const createUserDataMock = {
+  username: 'username',
+  password: 'Password1'
+}
+
+export const createUserResponseMock = {
+  id: 'clak46hxp0000o89qiv0tiggt',
+  username: 'username',
+  accountId: 'clak46hxp0001o89q4byflyd5',
+  token: 'validtoken'
+}
