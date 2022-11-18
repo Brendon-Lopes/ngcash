8a7bc3f622a5d3aa17d97e861045a196db11f230
Brendon <brendon.lopes.21@gmail.com>
2022-11-17 22:12:29 -0300

    test(server): :clown_face: add transactions mocks

---
+++ server/src/utils/mocks/transaction-mocks.ts
@@ -0,0 +1,21 @@
+export const recipientUserMock = {
+  id: 'clalpa41n0004qsrfgyzjqzb7',
+  username: 'user2',
+  accountId: 'clalpa41n0005qsrfhzzy3b0g'
+}
+
+export const senderUserMock = {
+  id: 'clalpa1ic0000qsrfoxmrm5sz',
+  username: 'user1',
+  accountId: 'clalpa1ic0001qsrfmffcslg7'
+}
+
+export const recipientAccountMock = {
+  id: 'clalpa41n0005qsrfhzzy3b0g',
+  balance: 100
+}
+
+export const senderAccountMock = {
+  id: 'clalpa1ic0001qsrfmffcslg7',
+  balance: 100
+}
