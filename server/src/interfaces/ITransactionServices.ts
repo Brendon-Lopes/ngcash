0fe9a4c0771df842ee0896ef0fe84749faa79890
Brendon <brendon.lopes.21@gmail.com>
2022-11-19 19:43:18 -0300

    feat(server): :sparkles: update interface ITransactionServices
    add new method 'readFilteredByDate'

--- server/src/interfaces/ITransactionServices.ts
+++ server/src/interfaces/ITransactionServices.ts
@@ -1,8 +1,10 @@
 import ICreateTransactionData from './ICreateTransactionData'
 import ICreateTransactionResponse from './ICreateTransactionResponse'
 import IReadTransactionResponse from './IReadTransactionResponse'
+import ITransactionFilter from './ITransactionFilter'
 
 export default interface ITransactionServices {
   create: (data: ICreateTransactionData, token: string) => Promise<ICreateTransactionResponse>
   read: (token: string) => Promise<IReadTransactionResponse[]>
+  readFilteredByDate: (filter: ITransactionFilter, token: string) => Promise<IReadTransactionResponse[]>
 }
