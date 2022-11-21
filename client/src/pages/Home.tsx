80326f1540b27080eb3afe5f23ae5ca6de1ae886
Brendon <brendon.lopes.21@gmail.com>
2022-11-21 14:36:09 -0300

    feat(client): :sparkles: add NavBar to home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,26 @@
+import { useEffect } from 'react'
+import { useCookies } from 'react-cookie'
+import { useNavigate } from 'react-router-dom'
+import { Nav } from '../components'
+
 export default function Home() {
+  const [cookies] = useCookies(['token'])
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    document.title = 'Home'
+
+    const { token } = cookies
+
+    if (token === undefined) {
+      navigate('/login')
+    }
+  }, [cookies])
+
   return (
     <div>
+      <Nav />
       <h1>Home</h1>
     </div>
   )
