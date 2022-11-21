b23d4c48774bf2f43c521ab8c97ad0e56ca35fa7
Brendon <brendon.lopes.21@gmail.com>
2022-11-21 17:50:21 -0300

    feat(client): :sparkles: update transactionServices

--- client/src/services/transactionServices.ts
+++ client/src/services/transactionServices.ts
@@ -1,3 +1,5 @@
+import { ITransactionFilterOptions } from '../interfaces'
+import ITransaction from '../interfaces/ITransaction'
 import { api } from '../providers/Api'
 
 const newTransaction = async (
@@ -21,4 +23,35 @@ const newTransaction = async (
   return response.data
 }
 
-export { newTransaction }
+const getTransactions = async (token: string): Promise<ITransaction[]> => {
+  const response = await api.get('/transactions', {
+    headers: {
+      Authorization: token,
+    },
+  })
+
+  return response.data
+}
+
+const getFilteredTransactions = async (
+  token: string,
+  filterOptions: ITransactionFilterOptions
+): Promise<ITransaction[]> => {
+  let endpoint: string
+
+  if (filterOptions.type === undefined) {
+    endpoint = `/transactions/filter?startDate=${filterOptions.startDate}&endDate=${filterOptions.endDate}`
+  } else {
+    endpoint = `/transactions/filter?startDate=${filterOptions.startDate}&endDate=${filterOptions.endDate}&type=${filterOptions.type}`
+  }
+
+  const response = await api.get(endpoint, {
+    headers: {
+      Authorization: token,
+    },
+  })
+
+  return response.data
+}
+
+export { newTransaction, getTransactions, getFilteredTransactions }
