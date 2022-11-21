edce9e35abfdb2fb0603c50d649d622c018b520e
Brendon <brendon.lopes.21@gmail.com>
2022-11-21 11:11:47 -0300

    feat(client): :sparkles: add NavBar component

---
+++ client/src/components/Nav/index.tsx
@@ -0,0 +1,9 @@
+import LogoNgcash from '../../assets/logo-ngcash.svg'
+
+export default function Nav() {
+  return (
+    <nav className="bg-gray-800">
+      <img src={LogoNgcash} alt="Logo NGCash" className="h-14" />
+    </nav>
+  )
+}
