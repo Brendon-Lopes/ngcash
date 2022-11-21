798509be80fb43cbbbef198e41a76a32b8cb2f08
Brendon <brendon.lopes.21@gmail.com>
2022-11-21 14:35:30 -0300

    feat(client): :sparkles: update NavBar
    add links and info

--- client/src/components/Nav/index.tsx
+++ client/src/components/Nav/index.tsx
@@ -1,9 +1,67 @@
 import LogoNgcash from '../../assets/logo-ngcash.svg'
+import { NavLink } from 'react-router-dom'
+import { useCookies } from 'react-cookie'
+import { jwt, transformPrice } from '../../utils'
+import { useEffect, useState } from 'react'
+import { userServices } from '../../services'
 
 export default function Nav() {
+  const [cookies, , removeCookie] = useCookies(['token'])
+
+  const [balance, setBalance] = useState('')
+
+  const handleLogout = () => {
+    removeCookie('token', { path: '/' })
+  }
+
+  const getUsername = () => {
+    const { token } = cookies
+
+    if (token !== undefined) {
+      const { username } = jwt.decodeToken(token)
+
+      return username
+    }
+  }
+
+  useEffect(() => {
+    if (cookies.token !== undefined) {
+      userServices
+        .getBalance(cookies.token)
+        .then((balance) => setBalance(balance))
+        .catch((err) => console.log(err))
+    }
+  }, [cookies])
+
+  if (cookies.token !== undefined) {
+    return (
+      <nav className="flex items-center justify-cente py-3 px-8 shadow-m bg-gray-800">
+        <div className="flex items-center gap-8">
+          <img src={LogoNgcash} alt="Logo NGCash" className="h-10" />
+          <p className="text-white">Olá, {getUsername()}</p>
+          <p className="text-white">Saldo: {transformPrice(balance)}</p>
+          <button
+            onClick={handleLogout}
+            className="bg-slate-100 py-1 px-4 rounded"
+          >
+            Sair
+          </button>
+        </div>
+      </nav>
+    )
+  }
+
   return (
-    <nav className="bg-gray-800">
-      <img src={LogoNgcash} alt="Logo NGCash" className="h-14" />
+    <nav className="flex items-center justify-cente py-3 px-8 shadow-m bg-gray-800">
+      <div className="flex items-center gap-8">
+        <img src={LogoNgcash} alt="Logo NGCash" className="h-10" />
+        <NavLink to="/login" className="text-white">
+          Login
+        </NavLink>
+        <NavLink to="/cadastro" className="text-white">
+          Criar conta
+        </NavLink>
+      </div>
     </nav>
   )
 }
