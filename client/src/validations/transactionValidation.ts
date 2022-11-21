6b3321a00589d0a4ef1b9ce90ec335b0cb8c0a1a
Brendon <brendon.lopes.21@gmail.com>
2022-11-21 16:26:41 -0300

    feat(client): :sparkles: add validation for new transactions

---
+++ client/src/validations/transactionValidation.ts
@@ -0,0 +1,17 @@
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const transactionValidationSchema = yup.object().shape({
+  creditedAccountUsername: yup
+    .string()
+    .required('Username é obrigatório')
+    .min(3, 'Username deve ter no mínimo 3 caracteres')
+    .matches(/^[a-zA-Z0-9]+$/, 'Username deve conter apenas letras e números'),
+
+  value: yup
+    .number()
+    .required('Valor é obrigatório')
+    .typeError('Valor deve conter apenas números e ponto'),
+})
+
+export const transactionValidation = yupResolver(transactionValidationSchema)
